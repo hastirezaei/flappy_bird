07fdd6901030e13c53e0ebc0408a54a855afdf24
hasti rezaei <144585692+hastirezaei@users.noreply.github.com>
2023-10-08 09:55:47 +0330

    Update game.py

---
+++ game.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+pygame.init()
+
+main_screen = pygame.display.set_mode((456, 645))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(85)
+    
